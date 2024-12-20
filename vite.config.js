724a321858c0555fc396e7dcbf43dee100993b71
kero <kero@comore.bi>
2024-12-20 17:00:32 +0900

    modify config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: process.env.GITHUB_PAGES ? 'sass2haml' : './',
+  plugins: [react()]
 })
